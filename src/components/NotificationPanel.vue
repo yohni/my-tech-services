af77d98f4bdbd3e423b746d228fc9ce5243292ba
yohni <yohni.123@gmail.com>
2020-07-23 15:49:07 +0700

    Fix bg panel

--- src/components/NotificationPanel.vue
+++ src/components/NotificationPanel.vue
@@ -30,6 +30,7 @@ export default {
 <style lang="scss" scoped>
 @import '../styles/theme.scss';
 .notif-wrapper{
+  background: #fff;
   overflow: hidden;
   display: flex;
   justify-content: center;
