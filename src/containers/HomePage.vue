148d80ad740541bc4308ba1866273ce6e0fa89a9
yohni <yohni.123@gmail.com>
2020-07-23 15:49:52 +0700

    Create header and highligth section

--- src/containers/HomePage.vue
+++ src/containers/HomePage.vue
@@ -1,7 +1,37 @@
 <template>
   <div>
     <NotificationPanel />
-    Halloaksjalksdjh
+    <header class="header">
+      <div class="header-wrapper">
+        <img 
+          class="header-logo" 
+          src="../assets/logo.png" 
+          alt="logo">
+        <h1>Hello! I'm Achmad Yohni W.F.</h1>
+        <h2>Consult, Design, and Develop Websites</h2>
+        <p>Have something great in mind? Feels me to contact me.</p>
+        <p>I'll help you to make it happen</p>
+
+        <button class="header-btn">LET'S MAKE CONTACT</button>
+      </div>
+    </header>
+    <div class="hl">
+      <h1 class="hl-title">How Can I Help You?</h1>
+      <div class="hl-sub">
+        <p>Our work then targeted, best practices outcomes social innovation synergy.</p>
+        <p>Venture philanthropy, revolutionary inclusive policymaker relief. User-centered
+program areas scale.</p>
+      </div>
+      <div class="hl-wrapper">
+        <div class="hl-item">
+          <div class="hl-item-header">
+            <div class="hl-item-title">
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -49,7 +79,8 @@ footer, header, hgroup, menu, nav, section {
 	display: block;
 }
 body {
-	line-height: 1;
+  line-height: 1;
+  background: #e5e5e5;
 }
 ol, ul {
 	list-style: none;
@@ -76,5 +107,96 @@ button {
   cursor: pointer;
 }
 
+@import '../styles/theme';
+
+.header{
+  background: url('../assets/header_bg.jpg');
+  background-size: cover;
+  position: relative;
+
+  .header-wrapper{
+    background: rgba($color: #004a75, $alpha: 0.5);
+    min-height: 568px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    color: #ffffff;
+    text-align: center;
+    padding: 0 24px;
+
+    .header-logo{
+      position: absolute;
+      top: 24px;
+      left: 24px;
+      width: 64px;
+      height: 64px;
+
+      @media (max-width: 320px) {
+        width: 40px;
+        height: 40px;
+        top: 12px;
+      }
+    }
+
+    h1{
+      font-weight: 400;
+      font-size: 32px;
+      margin-bottom: 12px;
+    }
+
+    h2{
+      font-weight: 600;
+      font-size: 24px;
+      margin-bottom: 12px;
+    }
+
+    p{
+      margin-bottom: 4px;
+    }
+
+    button.header-btn{
+      margin-top: 24px;
+      padding: 12px 24px;
+      background: rgba($color: #ffffff, $alpha: 0);
+      border: #ffffff solid 2px;
+      color: #ffffff;
+      font-weight: bold;
+      transition: all 0.25s;
+
+      &:hover{
+        color: $MyBlue;
+        background: rgba($color: #ffffff, $alpha: 1);
+      }
+    }
+  }
+}
+
+.hl{
+  padding: 24px;
+
+  .hl-title{
+    text-align: center;
+    margin-top: 24px;
+    margin-bottom: 24px;
+    font-weight: 400;
+    font-size: 32px;
+  }
+
+  .hl-sub{
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    line-height: 140%;
+    margin-bottom: 24px;
+
+    @media (min-width: 480px) {
+      p{
+        max-width: 40%;
+      }  
+    }
+  }
+}
 
 </style>
